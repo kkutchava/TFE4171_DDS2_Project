028cc04693fe972b5142883142ba9f9442f5b585
Alexandru Carbuneanu <ac@emlogic.no>
2025-04-14 19:50:52 +0200

    fhjf

--- tb/assertions_hdlc.sv
+++ tb/assertions_hdlc.sv
@@ -118,7 +118,7 @@ module assertions_hdlc (
   // 14. Rx_FrameSize should equal the exact number of bytes received in a frame (max. 126 bytes).
   property RX_FrameSize_Exact;
     @(posedge Clk) disable iff (!Rst)
-    (Rx_ValidFrame && $rose(Rx_NewByte)) |-> (Rx_FrameSize == byte_counter);
+    $rose(Rx_EoF) |-> (Rx_FrameSize == byte_counter);
   endproperty
 
   RX_FrameSize_Exact_Assert : assert property (RX_FrameSize_Exact) begin
